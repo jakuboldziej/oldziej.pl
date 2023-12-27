b3de0f95a4d3094be3a05a393cd31323b494fadc
jakuboldziej <jakub.oldziej@wp.pl>
2023-12-27 11:00:43 +0100

    darts

--- src/components/Darts/Keyboard.jsx
+++ src/components/Darts/Keyboard.jsx
@@ -15,8 +15,8 @@ function Keyboard({ params }) {
         <button className="input number" disabled={specialState[1] === "TRIPLE"} onClick={() => onclick(25)}>25</button>
         <button className="input number" disabled={specialState[0]} onClick={() => onclick(0)}>0</button>
         <button className="input special" style={{backgroundColor: "#dbff00"}} onClick={() => onclick('DRZWI')}>DRZWI</button>
-        <button className="input special" style={{backgroundColor: "#ffd100"}} onClick={() => onclick('DOUBLE')}>DOUBLE</button>
-        <button className="input special" style={{backgroundColor: "#ff8a00"}} onClick={() => onclick('TRIPLE')}>TRIPLE</button>
+        <button className="input special" style={{backgroundColor: "#ffd100", opacity: `${specialState[1] === 'DOUBLE' ? 0.8 : 1}`}} onClick={() => onclick('DOUBLE')}>DOUBLE</button>
+        <button className="input special" style={{backgroundColor: "#ff8a00", opacity: `${specialState[1] === 'TRIPLE' ? 0.8 : 1}`}} onClick={() => onclick('TRIPLE')}>TRIPLE</button>
         {/* <div className="input special" style={{backgroundColor: "#ff3800"}} onClick={() => handleRound('BACK')}>BACK</div> */}
       </div>
     </>
