dfb8288e493a2d7c023fccf43286ad9d080fc0f6
jakuboldziej <jakub.oldziej@wp.pl>
2024-06-14 15:26:07 +0200

    portfolio

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { CombinedContextProvider } from './context/CombinedContextProviders';
+import "/src/styles/home.scss"
+import "/src/styles/portfolio.scss"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <CombinedContextProvider>
--- frontend/src/pages/Portfolio/Home.jsx
+++ frontend/src/pages/Portfolio/Home.jsx
@@ -1,8 +1,36 @@
+import BoxReveal from '@/components/magicui/box-reveal'
+import { FadeText } from '@/components/magicui/fade-text'
+import Particles from '@/components/magicui/particles.tsx'
 import React from 'react'
+import { motion } from "framer-motion"
 
 function Home() {
   return (
-    <div className='text-2xl text-white flex items-center justify-center h-screen '>Page under development...</div>
+    <>
+      <div className='home-portfolio'>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+          className='particles fixed h-screen w-full'>
+          <Particles className='h-full' />
+        </motion.div>
+        <div className='home-portfolio-content h-screen flex flex-col items-center text-white'>
+          <div className='w-full h-80 mt-20'>
+            <div className='relative flex flex-col items-center gap-3 h-36'>
+              <FadeText className='text-5xl' text='Jakub' framerProps={{ show: { transition: { delay: 0.3 } } }} />
+              <FadeText className='text-5xl' text='Ołdziejewski' framerProps={{ show: { transition: { delay: 0.6 } } }} />
+              <BoxReveal duration={1}>
+                Full-stack Developer
+              </BoxReveal>
+            </div>
+          </div>
+          <div className='flex flex-col items-center w-full h-80'>
+            <span>asdf</span>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
