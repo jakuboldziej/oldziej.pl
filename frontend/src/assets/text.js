f57d4c276a85bba4dfa7439b19db82547b4ce10d
jakuboldziej <jakub.oldziej@wp.pl>
2024-07-19 14:33:13 +0200

    teksty

--- frontend/src/assets/text.js
+++ frontend/src/assets/text.js
@@ -16,7 +16,7 @@ export const textData = {
     },
     "about": {
       "descBreaking": "Kultura, w której się wychowałem, tańczę od 2010 roku.",
-      "descBouldering": "Swoją przygodę ze wspinaczką rozpocząłem w 2019 roku i od tego czasu osiągnąłem poziom wspinacza około 6C.",
+      "descBouldering": "Swoją przygodę ze wspinaczką rozpocząłem w 2019 roku i od tego czasu osiągnąłem poziom wspinacza około 6C+.",
       "descFormulaOne": "Mój szwagier wprowadził mnie w świat Formuły 1, w weekendy, z wyjątkiem tańczenia breaka, lubię patrzeć, jak kierowcy jeżdżą w kółko.",
       "ctaBreaking": "Zobacz więcej",
       "ctaBouldering": "Gdzie się wspinam?",
@@ -50,7 +50,7 @@ export const textData = {
     },
     "about": {
       "descBreaking": "A culture that I grew up in, dancing since 2010.",
-      "descBouldering": "I started my climbing journey in 2019 and since then I've become around 6C level climber.",
+      "descBouldering": "I started my climbing journey in 2019 and since then I've become around 6C+ level climber.",
       "descFormulaOne": "My brother-in-law introduced me to the world of Formula 1, on the weekends except for dancing in breaking events I like to watch people drive in circles.",
       "ctaBreaking": "See more of me",
       "ctaBouldering": "Where do I climb?",
