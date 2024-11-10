9c795d47e7148254118a89febc9e34de6ce115ed
jakuboldziej <jakub.oldziej@wp.pl>
2024-11-10 20:05:45 +0100

    share link, opti dropdown

---
+++ frontend/src/components/Home/Cloud/CustomFileDropdown.jsx
@@ -0,0 +1,54 @@
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuPortal, DropdownMenuSeparator, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger, DropdownMenuTrigger } from '@/components/ui/shadcn/dropdown-menu';
+import { FileArchive, FileDown, Heart, HeartOff, Info, Move, PencilLine, Search, SquareArrowDown, Trash2, Files, FileSymlink, Link } from 'lucide-react';
+import { renderFile } from "@/components/Home/Cloud/utils";
+
+function CustomFileDropdown({ props }) {
+  const { filesViewType } = props;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className={`dropdown-trigger rounded-full hover:text-slate-400 ${filesViewType === "list" ? "mr-4" : "bg-slate-700"}`}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-ellipsis hover:cursor-pointer"><circle cx="12" cy="12" r="1" /><circle cx="19" cy="12" r="1" /><circle cx="5" cy="12" r="1" /></svg>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem onClick={() => renderFile(file.filename)} className='gap-2'><Search /> Preview</DropdownMenuItem>
+        <DropdownMenuSub>
+          <DropdownMenuSubTrigger className="gap-2">
+            <FileDown />
+            <span>Download...</span>
+          </DropdownMenuSubTrigger>
+          <DropdownMenuPortal>
+            <DropdownMenuSubContent>
+              <DropdownMenuItem onClick={() => handleDownloadFile(file.filename)} className='gap-2'><SquareArrowDown /> Standard</DropdownMenuItem>
+              <DropdownMenuItem onClick={() => handleDownloadFile(file.filename)} className='gap-2'><FileArchive /> As a ZIP file</DropdownMenuItem>
+            </DropdownMenuSubContent>
+          </DropdownMenuPortal>
+        </DropdownMenuSub>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => handleShareLink(file.filename)} className='gap-2'><Link />Share link</DropdownMenuItem>
+        <DropdownMenuItem disabled onClick={() => handleShareFile(file.filename)} className='gap-2'><FileSymlink />Share file</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => handleOpeningDialog(file, "showInfo")} className='gap-2'><Info />Info</DropdownMenuItem>
+        <DropdownMenuItem
+          onSelect={(e) => e.preventDefault()}
+          onMouseLeave={() => setIsHovered((prev) => ({ ...prev, heart: false }))}
+          onMouseEnter={() => setIsHovered((prev) => ({ ...prev, heart: true }))}
+          onClick={() => handleFavoriteFile(file)}
+          className='gap-2'>
+          {file.favorite ?
+            isHovered.heart ? <HeartOff /> : <Heart color='#ff0000' />
+            : isHovered.heart ? <Heart color='#ff0000' /> : <Heart color='#ffffff' />
+          }
+          Favorite
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => handleOpeningDialog(file, "changeDataName")} className='gap-2'><PencilLine />Rename</DropdownMenuItem>
+        <DropdownMenuItem disabled className='gap-2'><Move />Move...</DropdownMenuItem>
+        <DropdownMenuItem disabled className='gap-2'><Files />Copy</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => handleDeleteFile(file)} className='gap-2'><Trash2 />Delete</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export default CustomFileDropdown
